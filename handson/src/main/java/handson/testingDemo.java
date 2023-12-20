72c1ef92fef76a970666d3a492f75a06862311e0
avisekg09 <avisekg09@gmail.com>
2023-12-21 01:03:39 +0530

    first commit
    first commit

---
+++ handson/src/main/java/handson/testingDemo.java
@@ -0,0 +1,11 @@
+package handson;
+
+import org.testng.annotations.Test;
+
+public class testingDemo {
+	@Test
+	public void testingLoginPage() {
+		System.out.println(".........Hello..........");
+	}
+
+}
